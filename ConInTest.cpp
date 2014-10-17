318c085dc0621a7cc66f242e1bebd8305c8dd611
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-10-18 03:13:08 +0400

    Bug with pasting '?' located

--- ConInTest.cpp
+++ ConInTest.cpp
@@ -7,60 +7,7 @@
 #include <tchar.h>
 #include <stdio.h>
 
-int help()
-{
-	return 1;
-}
-
-int ReadByChar()
-{
-	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	INPUT_RECORD rc = {};
-	DWORD nRead = 0, nWrite = 0;
-	int iKeyDown = 0, iKeyUp = 0, iMouse = 0, iOther = 0;
-	bool bLoop = true;
-
-	while (bLoop)
-	{
-		if (ReadConsoleInputW(hIn, &rc, 1, &nRead) && nRead)
-		{
-			switch (rc.EventType)
-			{
-			case KEY_EVENT:
-				if (rc.Event.KeyEvent.bKeyDown)
-				{
-					if (rc.Event.KeyEvent.uChar.UnicodeChar == 27)
-					{
-						bLoop = false;
-						break;
-					}
-					iKeyDown++;
-					if (rc.Event.KeyEvent.uChar.UnicodeChar == L'\r')
-						rc.Event.KeyEvent.uChar.UnicodeChar = L'\n';
-					WriteConsoleW(hOut, &rc.Event.KeyEvent.uChar.UnicodeChar, 1, &nWrite, NULL);
-					//Sleep(1); // simulate small delay
-				}
-				else
-				{
-					iKeyUp++;
-				}
-				break;
-			case MOUSE_EVENT:
-				// Just skip them for now
-				iMouse++;
-				break;
-			default:
-				//wprintf(L"{{{%u}}}", rc.EventType);
-				iOther++;
-			}
-		}
-	}
-
-	wprintf(L"\nStatistics: KeyDown %i, KeyUp %i, Mouse %i, Other %i\n", iKeyDown, iKeyUp, iMouse, iOther);
-
-	return 1;
-}
+#define TEXT_STR L"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ|||"
 
 int ReadFromFile()
 {
@@ -70,6 +17,10 @@ int ReadFromFile()
 	char szLine[4096+1];
 
 	SetConsoleMode(hIn, 1);
+
+	GetConsoleMode(hIn, &nRead);
+	GetConsoleMode(hOut, &nWrite);
+	printf("Console modes: In=0x%02X, Out=0x%02X\n", nRead, nWrite);
 
 	while (true)
 	{
@@ -81,95 +32,72 @@ int ReadFromFile()
 			SetConsoleTextAttribute(hOut, szLine[i]==L'?' ? 0x0C : 0x07);
 			WriteConsoleA(hOut, szLine+i, 1, &nWrite, NULL);
 		}
+		WriteConsoleA(hOut, "\n", 1, &nWrite, NULL);
 	}
-
-	//wprintf(L"\nStatistics: KeyDown %i, KeyUp %i, Mouse %i, Other %i\n", iKeyDown, iKeyUp, iMouse, iOther);
 
 	return 1;
 }
 
-int WriteStream(int iCharCount)
+int WriteStream()
 {
 	TCHAR szExe[MAX_PATH+16] = {}; GetModuleFileName(NULL, szExe, MAX_PATH);
 	TCHAR* pszName = _tcsrchr(szExe, _T('\\')); *(pszName++) = 0;
-	//_tcscat(pszName, L" /INCHAR");
 	_tcscat(pszName, L" /INLINE");
 	STARTUPINFO si = {sizeof(si)}; PROCESS_INFORMATION pi = {};
 	BOOL bCreate = CreateProcess(NULL, pszName, NULL, NULL, TRUE, 0, NULL, szExe, &si, &pi);
 
 	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO csbi = {};
-	GetConsoleScreenBufferInfo(hOut, &csbi);
-	int iWidth = csbi.dwSize.X; // csbi.srWindow.Right - csbi.srWindow.Left - 1;
-	INPUT_RECORD* prc = new INPUT_RECORD[iWidth*2];
-	DWORD nWritten = 0;
+
+	wchar_t szText[] = TEXT_STR;
+	const int nLen = lstrlen(szText);
+	INPUT_RECORD* prc = new INPUT_RECORD[nLen*2];
+	DWORD nWritten = 0, nWrite;
 
 	SetConsoleTitle(_T("Writing to console input buffer"));
 
-	int iAllWritten = 0;
-	int iRow = 0;
-	while (iCharCount > 0)
+	for (int i = 0; i < nLen; i++)
 	{
-		wchar_t ch = L'a' + ((iRow++) % 26);
+		wchar_t ch = szText[i];
 		INPUT_RECORD rc = {KEY_EVENT};
 		rc.Event.KeyEvent.bKeyDown = TRUE;
 		rc.Event.KeyEvent.dwControlKeyState = 0;
 		rc.Event.KeyEvent.uChar.UnicodeChar = ch;
 		rc.Event.KeyEvent.wVirtualKeyCode = 0;
 		rc.Event.KeyEvent.wVirtualScanCode = 0;
-		for (int i = 0; i < iWidth; i++)
-		{
-			prc[i*2] = rc;
-			prc[i*2+1] = rc;
-			prc[i*2+1].Event.KeyEvent.bKeyDown = FALSE;			
-		}
-		//prc[iWidth*2] = rc;
-		//prc[iWidth*2].Event.KeyEvent.uChar.UnicodeChar = L'\n';
-
-		nWritten = 0;
-		BOOL bWritten = WriteConsoleInputW(hIn, prc, iWidth*2+1, &nWritten);
-		iAllWritten += nWritten;
-		iCharCount -= nWritten;
-		wsprintfW(szExe, L"Writing to console input buffer: %i events done, %i left", iAllWritten, iCharCount);
-		SetConsoleTitleW(szExe);
-
-		if (!bWritten || ((iWidth*2+1) != nWritten))
-		{
-			wsprintfW(szExe, L"<<<WriteConsoleInputW failed, was written %i events, code %u>>>", iAllWritten, GetLastError());
-			SetConsoleTitleW(szExe);
-			break;
-		}
+		prc[i*2] = rc;
+		prc[i*2+1] = rc;
+		prc[i*2+1].Event.KeyEvent.bKeyDown = FALSE;
 	}
 
-	INPUT_RECORD rc = {KEY_EVENT};
-	rc.Event.KeyEvent.bKeyDown = TRUE;
-	rc.Event.KeyEvent.uChar.UnicodeChar = 27;
-	WriteConsoleInputW(hIn, &rc, 1, &nWritten);
+	int iAllWritten = 0;
+
+	while (MessageBox(NULL, L"Press <Retry> to paste\n\n" TEXT_STR, L"ConInTest", MB_RETRYCANCEL|MB_SYSTEMMODAL) == IDRETRY)
+	{
+		nWritten = 0;
+		//BOOL bWritten = WriteConsoleInputW(hIn, prc, nLen*2, &nWritten);
+		for (int i = 0; i < nLen*2; i++)
+		{
+			nWrite = 0;
+			if (WriteConsoleInputW(hIn, prc+i, 1, &nWrite))
+				nWritten+=nWrite;
+		}
+		iAllWritten += nWritten;
+		wsprintfW(szExe, L"Writing to console input buffer: %i events, %i in last step", iAllWritten, nWritten);
+		SetConsoleTitle(szExe);
+	}
 
 	SetConsoleTitle(_T("Done"));
-
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	//wprintf(L"\nDone, press Enter to exit");
-	//getchar();
-	MessageBox(NULL, _T("Press OK to exit"), _T("ConInTest"), MB_OK);
 	return 1;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int iRc = 1;
-	TCHAR *pch;
-
-	if (argc < 2 || _tcscmp(argv[1], _T("/?")) == 0)
-		iRc = help();
-	else if (_tcsicmp(argv[1], _T("/INCHAR")) == 0)
-		iRc = ReadByChar();
+	if (argc < 2 || _tcsicmp(argv[1], _T("/FLOOD")) == 0)
+		iRc = WriteStream();
 	else if (_tcsicmp(argv[1], _T("/INLINE")) == 0)
 		iRc = ReadFromFile();
-	else if (_tcsicmp(argv[1], _T("/FLOOD")) == 0)
-		iRc = WriteStream(argc>=3 ? _tcstol(argv[2], &pch, 10) : 8000);
-
 	return iRc;
 }
 
